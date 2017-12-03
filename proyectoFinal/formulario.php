5f8cfa145e2408184b13377120ac79695de5317c
RaulChan <34172071+RaulChan@users.noreply.github.com>
2017-12-03 14:58:29 -0500

    Formulario parte 2 de 2

--- proyectoFinal/formulario.php
+++ proyectoFinal/formulario.php
@@ -7,14 +7,20 @@
 <body>
 
 <?php
-$id_obt;
 
 
 $conexion=mysqli_connect("localhost","root","","login");
+$resultado=mysqli_query($conexion,"SELECT * FROM usuarios where sesion='Iniciada'");
+while($fila=mysqli_fetch_array($resultado)){
+$id_obt=$fila[0];
+$nombre_obt=$fila[1];
+$apellido_obt=$fila[2];
+$direccion_obt=$fila[3];
+$estado_obt=$fila[8];
+}
 
-$resultado=mysqli_query($conexion,"select * from usuarios where sesion=\"Iniciada\"");
 
-$fila=mysqli_fetch_array($resultado);
+
 //echo "Formulario a llenar<br>";
 
 //Inicio de formulario-------------------------------------------------------->
@@ -25,27 +31,27 @@ echo "<caption><strong>FORMULARIO</strong><br><br></caption>";
 //Fila 1 -------------------------------------------->
 echo "<tr>";
 echo "<th>ID</th>";
-echo "<td><input type='Text' name='CID'></td>";
+echo "<td><input type='Text' name='CID' value='$id_obt' readonly ></td>";
 echo "</tr>";
 //Fila 2----------------------------------------------->
 echo "<tr>";
 echo "<th>Nombre</th>";
-echo "<td><input type='Text' name='CNombre'></td>";
+echo "<td><input type='Text' name='CNombre' value='$nombre_obt' readonly></td>";
 echo "</tr>";
 // Fila 3---------------------------------------------->
 echo "<tr>";
 echo "<th>Apellido</th>";
-echo "<td><input type='Text' name='CApellido'></td>";
+echo "<td><input type='Text' name='CApellido' value='$apellido_obt' readonly></td>";
 echo "</tr>";
 //Fila 4----------------------------------------------->
 echo "<tr>";
 echo "<th>Dirección</th>";
-echo "<td><input type='Text' name='CDireccion'></td>";
+echo "<td><input type='Text' name='CDireccion' value='$direccion_obt' readonly></td>";
 echo "</tr>";
 //Fila 5----------------------------------------------->
 echo "<tr>";
 echo "<th>Carrera</th>";
-echo "<td><input type='Text' name ='CCarrera'></td>";
+echo "<td><input type='Text' name ='CCarrera' ></td>";
 echo "</tr>";
 //Fila 6----------------------------------------------->
 echo "<tr>";
@@ -58,7 +64,14 @@ echo "<th>Gustos</th>";
 echo "<td><input type='Text' name ='CGustos'></td>";
 echo "</tr>";
 
-//Fila 8 ----------------------------------------------->
+//Fila 8----------------------------------------------->
+echo "<tr>";
+echo "<th>Estado</th>";
+echo "<td><input type='Text' name ='CEstado' value='$estado_obt' readonly></td>";
+echo "</tr>";
+
+
+//Fila 9----------------------------------------------->
 echo "<tr>";
 echo "<th colspan='2'><input type='Submit' name='BEnviar' value='Enviar '><input type='Reset' name='BReset' value='Reset'></th>";
 echo "</tr>";
@@ -67,7 +80,7 @@ echo "</tr>";
 
 //Fin del formulario----------------------------------------------------------->
 
-mysqli_query($conexion,"update usuarios set estado=\"Enviada\" where id=$fila[0]");
+
 
 ?>
 
--- proyectoFinal/verificarFormulario.php
+++ proyectoFinal/verificarFormulario.php
@@ -2,17 +2,25 @@
 <body>
 <?php
 $conexion=mysqli_connect("localhost","root","","login");
-$resultado=mysqli_query($conexion,"SELECT * FROM usuarios");
-while($fila=mysqli_fetch_array($resultado)){
-if($fila[7]=='Iniciada'){
 
+$id_nuevo=$_POST["CID"];
+$nombre_nuevo=$_POST["CNombre"];
+$apellido_nuevo=$_POST["CApellido"];
+$direccion_nuevo=$_POST["CDireccion"];
+$carrera_nuevo=$_POST["CCarrera"];
+$extraEscolar_nuevo=$_POST["CExtra"];
+$gustos_nuevo=$_POST["CGustos"];
+$estado_nuevo=$_POST["CEstado"];
 
+if(isset($_POST["BEnviar"])){
+  $resultado=mysqli_query($conexion,"INSERT INTO formularios(id,nombre,apellido,direccion,carrera,extraescolar,gustos,estado) values($id_nuevo,'$nombre_nuevo','$apellido_nuevo','$direccion_nuevo','$carrera_nuevo','$extraEscolar_nuevo','$gustos_nuevo','$estado_nuevo')");
+  if(!$resultado){
+ echo "Error al enviar los datos".mysqli_error($conex);
 
-  
+  }else{
+mysqli_query($conexion,"update usuarios set estado=\"Enviada\" where sesion='Iniciada'");
+echo "Datos Enviados";
 }
-
-
-
 }
 
 ?>
