5e579437bfcc0353b42dd16dd7726ea9a7c65db0
dogobones <jrsierrahernandez@gmail.com>
2017-12-03 15:06:24 -0500

    Cosas

---
+++ proyectoFinal/actualizarAlumnos.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+<title></title>
+</head>
+
+<body>
+
+<?php
+
+$conexion=mysqli_connect("localhost","root","","login");
+
+$usuario=mysqli_query($conexion,"select * from usuarios where sesion='Iniciada'");
+
+$filaUsuario=mysqli_fetch_array($usuario);
+
+$resultado=mysqli_query($conexion,"select * from formularios where extraescolar='$filaUsuario[9]'");
+
+if(isset($_POST["salir"])) header("Location: login.php");
+
+else {
+	
+	$f=0;
+	  
+	while($fila=mysqli_fetch_array($resultado)) {
+		
+		mysqli_query($conexion,"update formularios set estado='$_POST('estado$f')' where id=$fila[0]");
+			  
+		$f++;
+	
+	}
+	
+	header("Location: paginaProfesor.php");
+	
+}
+
+?>
+
+</body>
+
+</html>
--- proyectoFinal/paginaProfesor.php
+++ proyectoFinal/paginaProfesor.php
@@ -18,6 +18,8 @@ $resultado=mysqli_query($conexion,"select * from formularios where extraescolar=
 
 echo "<center>$filaUsuario[1] EXTRAESCOLAR ENCARGADO $filaUsuario[9]<br><br></center>";
 
+echo "<form action='actualizarAlumnos' method='POST'>";
+
 echo "<caption><center><strong>Alumnos</strong><br></center></caption>";
 
 echo "<table align=center border=2>";
@@ -26,12 +28,14 @@ echo "<tr>
 		<th>Id</th><th>Nombre</th><th>Apellido</th><th>Dirección</th><th>Carrera</th><th>Gustos</th><th>Estado</th>
       </tr>";
 	  
+$f=0;
+	  
 while($fila=mysqli_fetch_array($resultado)) {
 	
 	echo "<tr>
 			<th>$fila[0]</th><th>$fila[1]</th><th>$fila[2]</th><th>$fila[3]</th><th>$fila[4]</th><th>$fila[6]</th>
 			<th>
-			<select name='estado'>
+			<select name='estado$f'>
 			<option value='enviada'>Enviada</option>
 			<option value='aceptada'>Aceptada</option>
 			<option value='rechazada'>Rechazada</option>
@@ -40,12 +44,12 @@ while($fila=mysqli_fetch_array($resultado)) {
 			</select>
 			</th>
 		  </tr>";
+		  
+    $f++;
 	
 }
 
 echo "</table>";
-
-echo "<form action='actualizarAlumnos' method='POST'>";
 
 echo "<center>
 		<input type='submit' name='actualizar' value='Actualizar'>
