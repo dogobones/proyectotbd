94e1ed6cc2812e8f6da1f636652ba095975660e0
RaulChan <34172071+RaulChan@users.noreply.github.com>
2017-12-03 13:13:43 -0500

    EstructuraBasica Formulario
    Formulario parte 1 de 2

--- proyectoFinal/formulario.php
+++ proyectoFinal/formulario.php
@@ -13,37 +13,63 @@ $conexion=mysqli_connect("localhost","root","","login");
 $resultado=mysqli_query($conexion,"select * from usuarios where sesion=\"Iniciada\"");
 
 $fila=mysqli_fetch_array($resultado);
+//echo "Formulario a llenar<br>";
 
-/*echo "<caption><strong>FORMULARIO</strong><br><br></caption>";
+//Inicio de formulario-------------------------------------------------------->
+echo "<form >";
+echo "<table border=2>";
+//Titulo de la tabla
+echo "<caption><strong>FORMULARIO</strong><br><br></caption>";
+//Fila 1 -------------------------------------------->
+echo "<tr>";
+echo "<th>ID</th>";
+echo "<td><input type='Text' name='CID'></td>";
+echo "</tr>";
+//Fila 2----------------------------------------------->
+echo "<tr>";
+echo "<th>Nombre</th>";
+echo "<td><input type='Text' name='CNombre'></td>";
+echo "</tr>";
+// Fila 3---------------------------------------------->
+echo "<tr>";
+echo "<th>Apellido</th>";
+echo "<td><input type='Text' name='CApellido'></td>";
+echo "</tr>";
+//Fila 4----------------------------------------------->
+echo "<tr>";
+echo "<th>Dirección</th>";
+echo "<td><input type='Text' name='CDireccion'></td>";
+echo "</tr>";
+//Fila 5----------------------------------------------->
+echo "<tr>";
+echo "<th>Carrera</th>";
+echo "<td><input type='Text' name ='CCarrera'></td>";
+echo "</tr>";
+//Fila 6----------------------------------------------->
+echo "<tr>";
+echo "<th>ExtraEscolar</th>";
+echo "<td><input type='Text' name ='CExtra'></td>";
+echo "</tr>";
+//Fila 7----------------------------------------------->
+echo "<tr>";
+echo "<th>Gustos</th>";
+echo "<td><input type='Text' name ='CGustos'></td>";
+echo "</tr>";
 
-echo "<form action=\"crearFormulario.php\" method=\"post\">";
-
-echo "<table>";
 
 
-echo "<tr>
-		<th>USUARIO</th>
-	    <td><input type=\"text\" name=\"user\" value=\"\"></td>
-      </tr>";
- 
-echo "<tr>
-		<th>CONTRASENIA</th>
-		<td><input type=\"password\" name=\"password\" value=\"\"></td>
-      </tr>";
 
-echo "<tr>
-		<th></th>
-        <td><input type=\"submit\" name=\"ingresar\" value=\"Sign in\"></td>
-      </tr>";
- 
 echo "</table>";
+echo "</form>";
 
-echo "</form>";*/
 
-echo "Formulario a llenar";
+
+
+
+//Fin del formulario----------------------------------------------------------->
 
 mysqli_query($conexion,"update usuarios set estado=\"Enviada\" where id=$fila[0]");
-	
+
 ?>
 
 </body>
